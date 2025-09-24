afd90a279d72843d2b4f2b5906f355bff441bfe1
gjasmeen <jasmeen.garcha@edu.sait.ca>
2025-09-23 20:54:09 -0600

    added week 2 pages
    added week 2 assignment pages with update to root/home page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+      <div className="page">
+        <h1>Jasmeen Garcha</h1>
+        <Link href="https://github.com/gjasmeen/cprg306-assignments.git"> Github</Link>
+      </div>
+    );
+  }
