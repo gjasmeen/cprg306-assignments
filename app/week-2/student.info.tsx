6bac2e4410c64710b4e7e0ca8dca51b4232f476f
gjasmeen <jasmeen.garcha@edu.sait.ca>
2026-01-26 20:07:57 -0700

    week2
    week2

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";   
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <p>Student Name: Jasmeen Garcha</p>
+      <Link href="https://github.com/gjasmeen/cprg306-assignments.git"> Github</Link>
+    </main>
+  );
+}
