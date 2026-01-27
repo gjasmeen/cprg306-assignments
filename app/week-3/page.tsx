c8602de3be8bd9b37f570b9946b4a11c5e047743
gjasmeen <jasmeen.garcha@edu.sait.ca>
2026-01-27 15:41:50 -0700

    week-3 commit

---
+++ app/week-3/page.tsx
@@ -0,0 +1,11 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main>
+      <p className= "text-center text-xl">Shopping List</p>
+      <ItemList />
+    </main>
+  );
+}
+
