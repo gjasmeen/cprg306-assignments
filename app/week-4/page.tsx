2679d43810f11ae346976a75e1ecd985d0025ef7
gjasmeen <jasmeen.garcha@edu.sait.ca>
2026-02-03 15:51:27 -0700

    week -4

---
+++ app/week-4/page.tsx
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-center font-extrabold text-pink-600">Add new Item</h1>
+      <NewItem />
+    </main>
+  );
+}
